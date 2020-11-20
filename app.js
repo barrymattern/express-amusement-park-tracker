bdbb5e63e806593f3c9479f3ce146b183b413e0b
Arjun <khyati@KHYATIs-Air.home>
2020-11-20 15:33:31 -0600

    projectt

--- app.js
+++ app.js
@@ -11,5 +11,22 @@ app.set('view engine', 'pug');
 app.use(morgan('dev'));
 
 app.use(routes);
+app.use((req,res,next) => {
+    const error = new Error("The requests page couldn't be found.");
+    error.status = 404
+    next(error)
+})
+
+app.use((err,req,res,next) => {
+    if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
+        // this is where the error would go for database
+    } else {
+        console.log(err)
+    }
+    next(err)
+})
+
+
+
 
 module.exports = app;
